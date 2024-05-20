6c537ceb59cab3ee863f1f9aa49caab6f7eb4816
Nanda Budi <nandabudi2003@gmail.com>
2024-05-20 15:12:18 +0700

    pygames

---
+++ pygames/games.py
@@ -0,0 +1,48 @@
+import random
+
+def main():
+    print("Pilih Salah Satu!")
+    print("1. Kertas")
+    print("2. Batu")
+    print("3. Gunting")
+    
+    pilih = int(input("Masukkan Pilihan Anda: "))
+    comp = random.randint(1, 3)
+
+    if pilih == 1:
+        print("Kamu Memilih Kertas")
+        if comp == 1:
+            print("Komputer Memilih Kertas")
+            print("Seri!")
+        elif comp == 2:
+            print("Komputer Memilih Batu")
+            print("Kamu Menang!")
+        else:
+            print("Komputer Memilih Gunting")
+            print("Komputer Menang!")
+    elif pilih == 2:
+        print("Kamu Memilih Batu")
+        if comp == 1:
+            print("Komputer Memilih Kertas")
+            print("Komputer Menang!")
+        elif comp == 2:
+            print("Komputer Memilih Batu")
+            print("Seri!")
+        else:
+            print("Komputer Memilih Gunting")
+            print("Kamu Menang!")
+    elif pilih == 3:
+        print("Kamu Memilih Gunting")
+        if comp == 1:
+            print("Komputer Memilih Kertas")
+            print("Kamu Menang!")
+        elif comp == 2:
+            print("Komputer Memilih Batu")
+            print("Komputer Menang!")
+        else:
+            print("Komputer Memilih Gunting")
+            print("Seri!")
+    else:
+        print("Pilihan Tidak Tersedia")
+
+main()
