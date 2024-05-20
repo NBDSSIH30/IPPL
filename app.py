97b0d0fee673ee57d49a7e160db4d2d333cec837
Nanda Budi <nandabudi2003@gmail.com>
2024-05-20 14:04:40 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+def luas_segitiga():
+    a = int(input("Masukkan Alas Segitiga :  "))
+    t = int(input("Masukkan Tinggi Segitiga :  "))
+    luas = a * t / 2
+    print("Luas Segitiga : ", luas)
+
+luas_segitiga()
+
+def luas_persegi_panjang():
+    p = int(input("Masukkan Panjang : "))
+    l = int(input("Masukkan Lebar :  "))
+    luasp = p * l
+    print("Luas Persegi Panjang : ", luasp)
+
+luas_persegi_panjang()
